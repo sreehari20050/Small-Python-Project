5186e9e44a3e577a3e418d796324760ccccec61d
Sreehari K B <sreehari22sreehari@gmail.com>
2024-12-05 17:56:38 +0530

    Create main.py

---
+++ Life in Weeks/main.py
@@ -0,0 +1,10 @@
+def life_in_weeks(age):
+    agee = 90
+    remainng = agee - age
+    x = remainng * 52
+    print(f"You have {x} weeks left.")
+  # your code here
+
+
+# Call your function with hard coded values
+life_in_weeks(12)
